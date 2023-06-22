f80f47445e6b7ae990327cd77774376dd4447e79
Joao Faria <joaorafaria@gmail.com>
2023-06-22 16:04:15 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import axios from "axios";
+import { useState } from "react";
+import { Route, Routes, NavLink } from "react-router-dom";
 
 function App() {
-  const useURL = "https://ih-crud-api.herokuapp.com/characters";
-  const [characters, setCharacters] = useState([]);
+  const baseURL = "https://ih-crud-api.herokuapp.com";
+
+  const [charactersArr, setCharactersArr] = useState(null);
 
   useEffect(() => {
     axios
-      .get(useURL)
+      .get(`${baseURL}/characters`)
       .then((response) => {
-        console.log(response.data[90]);
-        setCharacters(response.data);
+        setCharactersArr(response.data);
       })
       .catch((e) => console.log(e));
-  });
+  }, []);
+
+  const renderListOfCharacters = () => {
+    if (charactersArr === null) {
+      return <p>loading....</p>;
+    } else {
+      return charactersArr.map((characterObj) => {
+        return (
+          <div key={characterObj.id} className="character box">
+            Name: {characterObj.name} <br />
+            Weapon: {characterObj.weapon}
+          </div>
+        );
+      });
+    }
+  };
 
   return (
     <div className="App">
-      <h1>Number of characters {characters.length}</h1>
-      {characters.map((character) => {
-        return (
-          <div key={character.id}>
-            <ul>
-              <li>{character.name}</li>
+      <h1>React Charates App</h1>
 
-              <ul>
-                <li>{character.weapon}</li>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
 
-                <li>{character.occupation}</li>
-              </ul>
-            </ul>
-          </div>
-        );
-      })}
+      <Routes>
+        <Route path="/" element={renderListOfCharacters()} />
+        <Route path="/about" element={<p>Display About page</p>} />
+        <Route path="/contact" element={<p>Display Contact page</p>} />
+      </Routes>
     </div>
   );
 }
